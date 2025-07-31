7b68ae1ba5ac4f818d7779462cbcda30e8393506
Ramatoulaye81 <ramatoulaye0108@gmail.com>
2025-07-31 06:36:47 +0000

    Add files via upload

--- final_project/index.js
+++ final_project/index.js
@@ -11,7 +11,22 @@ app.use(express.json());
 app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
 
 app.use("/customer/auth/*", function auth(req,res,next){
-//Write the authenication mechanism here
+    // Vérifier si la session contient bien une autorisation
+    if (req.session.authorization) {
+        let token = req.session.authorization['accessToken'];
+
+        // Vérifier le token JWT avec la même clé utilisée au login
+        jwt.verify(token, "access", (err, user) => {
+            if (err) {
+                return res.status(403).json({ message: "User not authenticated" });
+            } else {
+                req.user = user; // On attache l’utilisateur décodé à la requête
+                next(); // Passe à la route suivante
+            }
+        });
+    } else {
+        return res.status(403).json({ message: "User not logged in" });
+    }
 });
  
 const PORT =5000;
